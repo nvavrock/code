ebcc417d7981b8f23cdd06d8dfa24861e23b8594
Nate <nvavrock@gmail.com>
2026-01-29 18:21:37 -0500

    Refactor UI layout in app.R for clarity

--- shiny/003-play-golf/app.R
+++ shiny/003-play-golf/app.R
@@ -40,35 +40,35 @@ model <- randomForest(play ~ ., data = weather, ntree = 500, mtry = 4, importanc
 
 ui <- fluidPage(theme = shinytheme("united"),
                 
-                # Page header
-                headerPanel('Play Golf?'),
+  # Page header
+  headerPanel('Play Golf?'),
                 
-                # Input values
-                sidebarPanel(
-                  HTML("<h3>Input parameters</h3>"),
+  # Input values
+  sidebarPanel(
+  HTML("<h3>Input parameters</h3>"),
                   
-                  selectInput("outlook", label = "Outlook:", 
-                              choices = list("Sunny" = "sunny", "Overcast" = "overcast", "Rainy" = "rainy"), 
-                              selected = "Rainy"),
-                  sliderInput("temperature", "Temperature:",
-                              min = 64, max = 86,
-                              value = 70),
-                  sliderInput("humidity", "Humidity:",
-                              min = 65, max = 96,
-                              value = 90),
-                  selectInput("windy", label = "Windy:", 
-                              choices = list("Yes" = "TRUE", "No" = "FALSE"), 
-                              selected = "TRUE"),
+  selectInput("outlook", label = "Outlook:", 
+              choices = list("Sunny" = "sunny", "Overcast" = "overcast", "Rainy" = "rainy"), 
+              selected = "Rainy"),
+  sliderInput("temperature", "Temperature:",
+              min = 64, max = 86,
+              value = 70),
+  sliderInput("humidity", "Humidity:",
+              min = 65, max = 96,
+              value = 90),
+  selectInput("windy", label = "Windy:", 
+              choices = list("Yes" = "TRUE", "No" = "FALSE"), 
+              selected = "TRUE"),
                   
-                  actionButton("submitbutton", "Submit", class = "btn btn-primary")
-                ),
+  actionButton("submitbutton", "Submit", class = "btn btn-primary")
+  ),
                 
-                mainPanel(
-                  tags$label(h3('Status/Output')), # Status/Output Text Box
-                  verbatimTextOutput('contents'),
-                  tableOutput('tabledata') # Prediction results table
+  mainPanel(
+  tags$label(h3('Status/Output')), # Status/Output Text Box
+  verbatimTextOutput('contents'),
+  tableOutput('tabledata') # Prediction results table
                   
-                )
+  )
 )
 
 ####################################
